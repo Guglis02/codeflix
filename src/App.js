5ac712791e7df66df6a366b401fec4dfd57cddcd
Guglis02 <gustavomf02@gmail.com>
2020-07-27 14:31:08 -0300

    Base da aplicação

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"This is an educational video describing all the different hacks that are performed in Season 1 of Mr Robot.  I have worked in the Information Security industry for over 10 years and I thought it would be educational to show scenes from this series and other things and provide a deeper insight to the actions being performed and the reality of the situations that exist."}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
